8c1bbb9bf254cdd65cd6339662b823f824dc6631
Ben Hernandez <benjammin234@gmail.com>
2020-01-15 10:38:55 -0800

    Adds snapshot tests, list.test.js still fails

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import renderer from 'react-test-renderer';
 
 describe('App component', () => {
-  it('render the UI as expected', () => {
+  it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
     ReactDOM.unmountComponentAtNode(div);
