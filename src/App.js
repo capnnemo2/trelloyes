3bce04a1fc585078545cff19a609c425b8857690
Ben Hernandez <benjammin234@gmail.com>
2020-01-15 10:17:03 -0800

    Copied code now works, thanks to copying alternative solution via tech coach

--- src/App.js
+++ src/App.js
@@ -1,37 +1,29 @@
 import React from 'react';
-import List from './List';
-import './App.css'
-import store from './store';
+import './App.css';
+import List from './List'
+import store from './store'
 
 class App extends React.Component {
-  // static defaultProps = {
-  //   store: {
-  //     lists: [],
-  //     allCards: {},
-  //   }
-  // };
 
   render() {
-    // const { store } = this.props
-    // console.log(store);
     return (
-      <main className='App'>
-        <header className='App-header'>
+      <main className="App">
+        <header className="App-header">
           <h1>Trelloyes!</h1>
         </header>
-        <div className='App-list'>
+        <div className="App-list">
           {store.lists.map(list => (
             <List
               key={list.id}
               header={list.header}
-              cards={list.cardsIds.map(id => store.allCards[id])}
+              cards={list.cardIds.map(id => store.allCards[id])}
             />
           ))}
         </div>
       </main>
-    )
+    );
   }
-}
 
+}
 
 export default App;
